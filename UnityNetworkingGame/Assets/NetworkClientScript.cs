18797a65d8bad45a0959f16fc439482e201174ae
Chris Chung <christopher0chung@gmail.com>
2016-09-22 08:46:22 -0400

    New Scene - Testing RPC and logging latency

---
+++ UnityNetworkingGame/Assets/NetworkClientScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkClientScript : Photon.MonoBehaviour {
+
+    public Vector3 netPos;
+    public Quaternion netRot;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            netPos = (Vector3)stream.ReceiveNext();
+            netRot = (Quaternion)stream.ReceiveNext();
+        }
+    }
+
+    [PunRPC]
+    public void 
+
+}
